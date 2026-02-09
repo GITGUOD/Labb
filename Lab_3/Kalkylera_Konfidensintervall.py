a64033134516556647e249bce43bf481b0c9dad9
Tonny Huynh <tonnyhuynh02@gmail.com>
2026-02-09 22:51:58 +0100

    Added confidenceInterval method

---
+++ Lab_3/Kalkylera_Konfidensintervall.py
@@ -0,0 +1,32 @@
+import numpy as np
+import scipy.stats as stats
+import pandas as pd
+
+def confidenceInterval(data, confidence=0.95):
+  mean = np.mean(data)
+  std_err = stats.sem(data)  # Standard error of the mean
+  return stats.t.interval(confidence=confidence, df=len(data)-1, loc=mean, scale=std_err)
+
+
+df = pd.read_csv("Lab_3/data.csv")
+
+#data = np.random.normal(loc=50, scale=10, size=1000)
+groupA_estimated = df[df["Group"] == "A"]["Estimate"]
+groupB_estimated = df[df["Group"] == "B"]["Estimate"]
+
+print("Grupp A (uppskattad tid):", confidenceInterval(groupA_estimated))
+print("Grupp B (uppskattad tid):", confidenceInterval(groupB_estimated))
+
+groupA_actual = df[df["Group"] == "A"]["Actual"]
+groupB_actual = df[df["Group"] == "B"]["Actual"]
+
+print("Grupp A (verklig tid):", confidenceInterval(groupA_actual))
+print("Grupp B (verklig tid):", confidenceInterval(groupB_actual))
+
+
+print(f"95% Confidence Interval: {confidenceInterval(df["Actual"])}")
+print(f"95% Confidence Interval: {confidenceInterval(df["Estimate"])}")
+
+
+
+#confidenceInterval(df, confidence=0.99)
